529cbc97077e59d138af386147d7d536454d03ad
Hoie Kim <github@hoie.kim>
2022-06-28 20:30:15 -0700

    Adds sorting states

--- src/client/components/TransactionsTable/index.tsx
+++ src/client/components/TransactionsTable/index.tsx
@@ -1,9 +1,22 @@
-import { useAppContext } from "client";
+import { Transaction } from "server";
+import { useAppContext, useLocalStorage } from "client";
 import TransactionRow from "./TransactionRow";
 import TransactionsHead from "./TransactionsHead";
 
+export type SortingOptions = { [k in keyof Transaction]?: "ascending" | "descending" };
+export type VisibilityOptions = { [k in keyof Transaction]?: boolean };
+
 const TransactionsTable = () => {
   const { transactions } = useAppContext();
+
+  const [sortingOptions, setSortingOptions] = useLocalStorage<SortingOptions>(
+    "sortingOptions",
+    {}
+  );
+  const [visibilityOptions, setVisibilityOptions] = useLocalStorage<VisibilityOptions>(
+    "visibilityOptions",
+    {}
+  );
 
   const transactionRows = Array.from(transactions.values()).map((e, i) => {
     return <TransactionRow key={i} transaction={e} />;
@@ -13,7 +26,14 @@ const TransactionsTable = () => {
     <div className="TransactionsTable">
       <div>Transactions:</div>
       <table>
-        <TransactionsHead />
+        <TransactionsHead
+          options={{
+            sortingOptions,
+            setSortingOptions,
+            visibilityOptions,
+            setVisibilityOptions,
+          }}
+        />
         <tbody>{transactionRows}</tbody>
       </table>
     </div>
