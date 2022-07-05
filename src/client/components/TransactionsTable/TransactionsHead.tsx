5b3419f473521736f9075bb3ea357612c0aa4d68
Hoie Kim <hoiekim@amazon.com>
2022-07-05 09:26:35 -0700

    Refactors sorting functions for reusability

--- src/client/components/TransactionsTable/TransactionsHead.tsx
+++ src/client/components/TransactionsTable/TransactionsHead.tsx
@@ -1,80 +1,54 @@
-import { Dispatch } from "react";
-import { SortingKey, SortingOptions, VisibilityOptions } from ".";
+import { SetSortBy, GetArrow } from "client";
+import { TransactionHeaders } from ".";
 
-interface Props {
-  options: {
-    sortingOptions: SortingOptions;
-    setSortingOptions: Dispatch<SortingOptions>;
-    visibilityOptions: VisibilityOptions;
-    setVisibilityOptions: Dispatch<VisibilityOptions>;
-  };
+interface Props<T> {
+  setSortBy: SetSortBy<T>;
+  getArrow: GetArrow<T>;
 }
 
-const TransactionsHead = ({ options }: Props) => {
-  const { sortingOptions, setSortingOptions, visibilityOptions, setVisibilityOptions } =
-    options;
-
-  const flipSortingByKey = (key: SortingKey) => {
-    const existingValue = sortingOptions.get(key);
-    const newValue = existingValue === "ascending" ? "descending" : "ascending";
-    sortingOptions.delete(key);
-    sortingOptions.set(key, newValue);
-    return setSortingOptions(new Map(sortingOptions));
-  };
-
-  const getSortingArrowSymbolByKey = (key: SortingKey) => {
-    switch (sortingOptions.get(key)) {
-      case "ascending":
-        return "↑";
-      case "descending":
-        return "↓";
-      default:
-        return "";
-    }
-  };
-
+const TransactionsHead = ({ setSortBy, getArrow }: Props<TransactionHeaders>) => {
   return (
     <thead>
       <tr>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("authorized_date")}>
-              Date {getSortingArrowSymbolByKey("authorized_date")}
+            <button onClick={() => setSortBy("authorized_date")}>
+              Date {getArrow("authorized_date")}
             </button>
           </div>
         </td>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("merchant_name")}>
-              Name {getSortingArrowSymbolByKey("merchant_name")}
+            <button onClick={() => setSortBy("merchant_name")}>
+              Name {getArrow("merchant_name")}
             </button>
           </div>
         </td>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("amount")}>
-              Amount {getSortingArrowSymbolByKey("amount")}
+            <button onClick={() => setSortBy("amount")}>
+              Amount {getArrow("amount")}
             </button>
           </div>
         </td>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("account")}>
-              Account {getSortingArrowSymbolByKey("account")}
+            <button onClick={() => setSortBy("account")}>
+              Account {getArrow("account")}
             </button>
           </div>
         </td>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("institution")}>
-              Institution {getSortingArrowSymbolByKey("institution")}
+            <button onClick={() => setSortBy("institution")}>
+              Institution {getArrow("institution")}
             </button>
           </div>
         </td>
         <td>
           <div>
-            <button onClick={() => flipSortingByKey("category")}>
-              Category {getSortingArrowSymbolByKey("category")}
+            <button onClick={() => setSortBy("category")}>
+              Category {getArrow("category")}
             </button>
           </div>
         </td>
--- src/client/lib/hooks/index.ts
+++ src/client/lib/hooks/index.ts
@@ -2,3 +2,4 @@ export * from "./cache";
 export * from "./context";
 export * from "./router";
 export * from "./sync";
+export * from "./sorter";
