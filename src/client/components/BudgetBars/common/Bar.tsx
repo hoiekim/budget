1e5cebd58c279bb0a03d40bfad4b506fea286da5
Hoie Kim <github@hoie.kim>
2022-09-12 10:09:35 -0700

    Fixes bug in budget bar graphics

--- src/client/components/BudgetBars/common/Bar.tsx
+++ src/client/components/BudgetBars/common/Bar.tsx
@@ -8,6 +8,7 @@ type Props = { ratio: number; unlabledRatio?: number } & DetailedHTMLProps<
 const Bar = ({ ratio, unlabledRatio, className, ...rest }: Props) => {
   const [numeratorWidth, setNumeratorWidth] = useState(0);
   const [unlabeledNumeratorWidth, setUnlabeledNumeratorWidth] = useState(0);
+  const [overflowedNumeratorWidth, setOverflowedNumeratorWidth] = useState(0);
 
   useEffect(() => {
     setNumeratorWidth(ratio * 100);
@@ -18,28 +19,47 @@ const Bar = ({ ratio, unlabledRatio, className, ...rest }: Props) => {
     };
   }, [ratio, unlabledRatio]);
 
-  const overCapped = ratio + (unlabledRatio || 0) >= 1;
-  const alertClass = overCapped ? "alert" : "";
+  const isOverCapped = ratio + (unlabledRatio || 0) > 1;
+  const overflowedRatio = Math.max(ratio + (unlabledRatio || 0) - 1, 0);
+  const alertClass = isOverCapped ? "alert" : "";
+
+  useEffect(() => {
+    if (numeratorWidth + unlabeledNumeratorWidth > 100) {
+      setTimeout(() => {
+        setOverflowedNumeratorWidth(Math.min(1, overflowedRatio) * 100);
+      }, 500);
+    }
+    return () => {
+      setOverflowedNumeratorWidth(0);
+    };
+  }, [numeratorWidth, unlabeledNumeratorWidth]);
 
   return (
     <div {...rest} className={[className || "", "Bar", alertClass].join(" ")}>
       <div className="contentWithoutPadding">
+        <div className={["background", alertClass, alertClass && "colored"].join(" ")} />
         <div
           style={{
             left: `calc(${numeratorWidth}% - 10px)`,
-            width: `calc(${unlabeledNumeratorWidth}% + 10px)`,
+            width: unlabeledNumeratorWidth && `calc(${unlabeledNumeratorWidth}% + 10px)`,
           }}
           className={["unlabeledNumerator", "colored", alertClass].join(" ")}
         />
         <div
           style={{
-            width:
-              Math.min(
-                100,
-                numeratorWidth > 100 ? numeratorWidth - 100 : numeratorWidth
-              ) + "%",
+            width: Math.min(100, numeratorWidth) + "%",
           }}
           className={["numerator", "colored", alertClass].join(" ")}
+        />
+        <div
+          style={{
+            width: Math.min(100, overflowedNumeratorWidth) + "%",
+          }}
+          className={[
+            "overflowedNumerator",
+            "colored",
+            overflowedRatio > 1 ? alertClass : "",
+          ].join(" ")}
         />
       </div>
     </div>
